962914c2c6dcbdf4653b2004bdf791317f8edaea
Nathaly <suki.vi.britania@gmail.com>
2022-04-09 15:04:59 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hola mundo con React</h1>
+      <p>Para más de un elemento hay que usar un fragment '{"<></>"}'</p>
+      <strong>Cálculo con javascript {5*6}</strong>
+    </>
+  );
+}
+
+export default App;
